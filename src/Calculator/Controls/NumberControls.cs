e5ab3ad97c7ab9f5708e313881d8aa788c258bac
iamnovik <92405752+iamnovik@users.noreply.github.com>
2024-06-15 16:28:38 +0300

    UI Added (#3)

---
+++ src/Calculator/Controls/NumberControls.cs
@@ -0,0 +1,38 @@
+﻿namespace Calculator.Controls;
+
+public class NumberControls
+{
+    private Button[] _numberButtons;
+    private TextBox _inputField;
+
+    public NumberControls(TextBox inputField, int width, int height, string[] numberTexts)
+    {
+        _inputField = inputField;
+        _numberButtons = new Button[numberTexts.Length];
+        int buttonWidth = width / 6;
+        int buttonHeight = height / 11;
+        for (int i = 0; i < _numberButtons.Length; i++)
+        {
+            _numberButtons[i] = new Button
+            {
+                Text = numberTexts[i],
+                Size = new Size(buttonWidth, buttonHeight),
+                Location = new Point(10 + (i % 3) * (buttonWidth + 5), 50 + (i / 3) * (buttonHeight + 5)),
+                Name = "Button" + numberTexts[i]
+            };
+            _numberButtons[i].Click += new EventHandler(NumberButton_Click);
+        }
+    }
+
+    public Button[] GetButtons()
+    {
+        return _numberButtons;
+    }
+    
+    private void NumberButton_Click(object sender, EventArgs e)
+    {
+        Button button = sender as Button;
+        _inputField.Text += button.Text;
+    }
+
+}
---
+++ src/Calculator/Controls/OperationControls.cs
@@ -0,0 +1,37 @@
+﻿namespace Calculator.Controls;
+
+public class OperationControls
+{
+    private Button[] _operationButtons;
+    private TextBox _inputField;
+
+    public OperationControls(TextBox inputField, int width, int height, string[] operationTexts)
+    {
+        _inputField = inputField;
+        _operationButtons = new Button[operationTexts.Length];
+        int buttonHeight = height / 11;
+        int buttonWidth = width / 6;
+        for (int i = 0; i < _operationButtons.Length; i++)
+        {
+            _operationButtons[i] = new Button
+            {
+                Text = operationTexts[i],
+                Size = new System.Drawing.Size(buttonWidth, buttonHeight),
+                Location = new System.Drawing.Point(18 * width/30, 50 + i * (buttonHeight + 5)),
+                Name = "Button" + operationTexts[i]
+            };
+            _operationButtons[i].Click += new EventHandler(OperationButton_Click);
+        }
+    }
+
+    public Button[] GetButtons()
+    {
+        return _operationButtons;
+    }
+    
+    private void OperationButton_Click(object sender, EventArgs e)
+    {
+        Button button = sender as Button;
+        _inputField.Text += button.Text;
+    }
+}
